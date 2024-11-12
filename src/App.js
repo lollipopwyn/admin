11d7b98f50f90bf5551b177216747a203a51cb67
lollipopwyn <rwewine@gmail.com>
2024-11-12 15:21:13 +0900

    241112_build_admin_관리자 페이지 첫 커밋_나라

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import AdminLogin from './components/adminLogin/AdminLogin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AdminLogin />
+      <div className="App">
+        <Dashboard />
+        <Header />
+        <Routes></Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
