c5f10bb267b8396070bad92cb18375c69bfe80dd
lollipopwyn <rwewine@gmail.com>
2024-11-14 02:22:48 +0900

    240113_chore_header_css구조 수정_나라

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
   return (
     <div className="header">
       <h2>La Poem Admin System</h2>
-      <button>로그아웃</button>
+      {/* <button>로그아웃</button> */}
     </div>
   );
 };
